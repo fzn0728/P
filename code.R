b51b8b7091ffe24f26b5b49e45d25a96d81927a1
Chandler <thames.ft@gmail.com>
2016-06-15 23:25:20 -0700

    Add files via upload

---
+++ code.R
@@ -0,0 +1,33 @@
+# Return Calculation
+# Zhongnan Fang
+# June 14, 2016
+library(zoo)
+setwd("C:/Users/Chandler/Desktop/PPC/Data")
+
+# stock.df <- read.csv("RawData.csv", header = TRUE, skip = 1)
+# stock.df <-subset(stock.df, select = c("date","SICCD","TICKER","PRC","RET","SHROUT"))
+# stock.list <- sort(unique(stock.df$TICKER))
+# stock.df$date_Q <- as.yearqtr(with(stock.df,date),format = "%m/%d/%Y")
+# stock.df$date_M <- as.yearmon(with(stock.df,date),format = "%m/%d/%Y")
+# stock.df$MKT <- stock.df$PRC*stock.df$SHROUT
+# stock.df$R <- 1 + as.numeric(as.character(stock.df$RET))
+# save(stock.df,file="./stock.Rdata")
+load("stock.Rdata")
+
+# Filter all stock data from the watchlist
+stock_list <- list(read.csv("stock_list.csv",header=TRUE))[[1]]
+stock.df <- stock.df[(stock.df$TICKER %in% stock_list[,]),]
+
+# drop all space and na data
+stock.df <- stock.df[!(is.na(stock.df$R)|is.na(stock.df$TICKER)),]
+
+# Generate quarterly calculation dataframe
+stock_Q <- aggregate(stock.df$R,by=list(TICKER = stock.df$TICKER,date_Q = stock.df$date_Q),FUN = prod)
+stock_M <- aggregate(stock.df$R,by=list(TICKER = stock.df$TICKER,date_M = stock.df$date_M),FUN = prod)
+
+write.csv(stock_Q,file='stock_Q.csv')
+write.csv(stock_M,file='stock_M.csv')
+
+z <- stock.df[stock.df$date_Q == stock.df$date_Q[1],]
+prod(z$R[1:100])
+z$R[1:100]
