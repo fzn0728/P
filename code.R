8189ca0ef2253836425084f41098f59287b8f80c
Chandler <thames.ft@gmail.com>
2016-06-16 18:12:47 -0700

    Add files via upload

--- code.R
+++ code.R
@@ -2,6 +2,7 @@
 # Zhongnan Fang
 # June 14, 2016
 library(zoo)
+library(tidyr)
 setwd("C:/Users/Chandler/Desktop/PPC/Data")
 
 # stock.df <- read.csv("RawData.csv", header = TRUE, skip = 1)
@@ -16,6 +17,7 @@ load("stock.Rdata")
 
 # Filter all stock data from the watchlist
 stock_list <- list(read.csv("stock_list.csv",header=TRUE))[[1]]
+
 stock.df <- stock.df[(stock.df$TICKER %in% stock_list[,]),]
 
 # drop all space and na data
@@ -25,9 +27,17 @@ stock.df <- stock.df[!(is.na(stock.df$R)|is.na(stock.df$TICKER)),]
 stock_Q <- aggregate(stock.df$R,by=list(TICKER = stock.df$TICKER,date_Q = stock.df$date_Q),FUN = prod)
 stock_M <- aggregate(stock.df$R,by=list(TICKER = stock.df$TICKER,date_M = stock.df$date_M),FUN = prod)
 
-write.csv(stock_Q,file='stock_Q.csv')
-write.csv(stock_M,file='stock_M.csv')
+# write.csv(stock_Q,file='stock_Q.csv')
+# write.csv(stock_M,file='stock_M.csv')
+# write.csv(stock_list,file='stock_list.csv')
 
-z <- stock.df[stock.df$date_Q == stock.df$date_Q[1],]
-prod(z$R[1:100])
-z$R[1:100]
+### Generate return dataframe 
+return_Q <- spread(stock_Q,date_Q,x)
+write.csv()
+
+### get wired data
+wired.stock.df <- stock.df[stock.df$R>1.1 | stock.df$R<0.9,]
+write.csv(wired.stock.df,file='wired_stock_df.csv')
+
+
+
